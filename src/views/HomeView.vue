25bf84c1a8bd041467d62659b12c41051dc3db57
Ilyes Mobarek <mobarektaharilyes@gmail.com>
2026-05-26 13:43:49 +0900

    feat(ui): integrate HazardsEditor in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import LossesEditor from '../components/editors/LossesEditor.vue';
+import HazardsEditor from '../components/editors/HazardsEditor.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <LossesEditor />
+    <HazardsEditor />
+  </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
+</style>
